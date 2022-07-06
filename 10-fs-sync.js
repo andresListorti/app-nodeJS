7e6d245b5b4f2581d55446cfb9c265aa4407d74a
andresListorti <andreslistorti@gmail.com>
2022-07-06 17:50:04 -0300

    hoy

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,13 +1,14 @@
 
 const { readFileSync, writeFileSync } = require('fs')
 
+
 const primero = readFileSync('./content/primero.txt', 'utf8')
 const segundo = readFileSync('./content/segundo.txt', 'utf8')
 
 console.log(primero, segundo);
 
 writeFileSync(
-'./content/terceroConWrite.txt', 
-`Buenoooooo ahora siiii creamos uno nuevo y agregamos los datos del anterior con ${primero} y tambien con el ${segundo}`,
-{flag: 'a'}
+'./content/terceroConWrite.txt', // primer parametro dir y sino esta la hace
+`aaa            aaagregarrrrcreamos uno nuevo y agregamos los datos del anterior con ${primero} y tambien con el ${segundo}`, //segundo el contenido
+{flag: 'a'} // tercer parametro, con esto hacemos que appendice y no reemplace el contenido, sin esto lo reemplaza a todo
 )
