237775db5a81d9c06f54773332a7a9c933bcd0bb
andresListorti <andreslistorti@gmail.com>
2022-07-25 18:40:45 -0300

    25-2

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 const { readFile } = require('fs');
 
-readFile('./')
+
+
+const getText = (path) => {
+    return new Promise((resolve, reject)=>{
+        
+        readFile('./content/primero.txt', 'utf-8', (err, data) => {
+            if(err) {
+                return
+            } else {
+                console.log(data)
+            }
+        })
+
+    })
+}
+
+
+const start = async() => {
+  try {
+    const first = await getText('./content/primero.txt')
+    console.log(first);
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+start()
+
+
+// getText('./content/primero.txt')
+//     .then((result)=> console.log(result))
+//     .catch((err)=> console.log(err))
